4a14cec446aaa43ccc10a93b9313e698fd5ed2d7
QuokeCola <quokecola@icloud.com>
2020-06-15 01:17:14 +0800

    Rename Keyboard to Keyboard View for distinguishing.

--- ThermoCalc/Keyboard/DecimalKeyboard.swift
+++ ThermoCalc/Keyboard/DecimalKeyboard.swift
@@ -8,16 +8,102 @@
 
 import UIKit
 
-class DecimalKeyboard: UIView {
-    
+class DecimalKeyboardView: UIView {
+    fileprivate func PortraitSetup() {
+        self.frame = CGRect(x: 0.0, y: 0.0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.5)
+        self.backgroundColor = UIColor(red: 214/255, green: 215/255, blue: 220/255, alpha: 1.0)
+        totalHeight = self.frame.height - getBottomHeight() - ImagineView.bounds.height
+        totalWidth  = UIScreen.main.bounds.width
+        
+        let buttonWidth  = totalWidth  * 0.3
+        let buttonHeight = totalHeight * 0.2
+        let spacingX = totalWidth * 0.025
+        let spacingY = totalHeight * 0.04
+        
+        for i in 0...9 {
+            var x = CGFloat(0.0)
+            var y = CGFloat(0.0)
+
+            let imagineHeight = self.ImagineView.bounds.height
+
+            if (i == 0) {
+                x = buttonWidth + 2 * spacingX
+                y = 3*buttonHeight + 4 * spacingY + imagineHeight
+            } else {
+                x = (spacingX + buttonWidth ) * CGFloat((i-1)%3) + spacingX
+                y = (spacingY + buttonHeight) * CGFloat((i-1)/3) + spacingY + imagineHeight
+            }
+            DecimalButton[i] = KeyboardButton(frame: CGRect(x: x, y: y, width: buttonWidth, height: buttonHeight))
+            DecimalButton[i]?.setTitle("\(i)", for: .normal)
+            DecimalButton[i]?.titleLabel!.font = UIFont.boldSystemFont(ofSize: (DecimalButton[i]?.bounds.height)! * 0.5)
+            self.addSubview(DecimalButton[i]!)
+            self.addSubview(ImagineView)
+        }
+        let DeleteButton = KeyboardButton(frame: CGRect(x: (spacingX+buttonWidth)*2+spacingX, y: (spacingY+buttonHeight)*3+spacingY+ImagineView.bounds.height, width: buttonWidth, height: buttonHeight))
+        DeleteButton.setImage(#imageLiteral(resourceName: "Backspace"), for: .normal)
+        DeleteButton.setTitleColor(UIColor.black, for: .normal)
+        DeleteButton.imageView?.tintColor = UIColor.black
+        let DotButton = KeyboardButton(frame: CGRect(x: spacingX, y: (spacingY+buttonHeight)*3+spacingY+ImagineView.bounds.height, width: buttonWidth, height: buttonHeight))
+        DotButton.setTitle(".", for: .normal)
+        DotButton.setTitleColor(UIColor.black, for: .normal)
+        DotButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: buttonHeight*0.5)
+        self.addSubview(DeleteButton)
+        self.addSubview(DotButton)
+    }
+    fileprivate func reloadImagineWords() {
+        self.ImagineView.backgroundColor = UIColor(red: 214/255, green: 215/255, blue: 220/255, alpha: 1.0)
+        self.ImagineView.delaysContentTouches = false
+        self.ImagineView.showsVerticalScrollIndicator = false
+        self.ImagineView.showsHorizontalScrollIndicator = false
+        ImagineView.subviews.map { $0.removeFromSuperview() }
+        var imagineButtons = [ImagineButton?](repeating: nil, count: imagineUnits.count)
+        let imagineButtonHeight = CGFloat(35.0)
+        let imagineSpacingX = CGFloat(20.0)
+        let imagineSpacingY = (ImagineView.bounds.height - imagineButtonHeight)/2.0
+        var x = imagineSpacingX
+        for i in 0...imagineButtons.count-1 {
+            imagineButtons[i] = ImagineButton(frame: CGRect(x: x, y: imagineSpacingY, width: CGFloat(imagineUnits[i].count)*20.0, height: imagineButtonHeight))
+            imagineButtons[i]?.setTitle(imagineUnits[i], for: .normal)
+            x += (imagineButtons[i]!.bounds.width + imagineSpacingX)
+        }
+        ImagineView.contentSize = CGSize(width: x, height: ImagineView.bounds.height)
+        for i in 0...imagineButtons.count-1 {
+            ImagineView.addSubview(imagineButtons[i]!)
+        }
+    }
+    var imagineUnits = [String](repeating: "Unit", count: 10)
+    var totalHeight = CGFloat(0.0)
+    var totalWidth  = CGFloat(0.0)
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.backgroundColor = UIColor(red: 210/255, green: 213/255, blue: 219/255, alpha: 0.0)
-        
+        if(UIDevice.modelName.contains(find: "iPad") || UIApplication.shared.statusBarOrientation == .landscapeLeft || UIApplication.shared.statusBarOrientation == .landscapeRight) {
+            
+        } else {
+            PortraitSetup()
+            reloadImagineWords()
+        }
     }
     
+    var DecimalButton = [KeyboardButton?](repeating: nil, count: 10)
+    var ImagineView = UIScrollView(frame: CGRect(x: 0.0, y: 0.0, width: UIScreen.main.bounds.width, height: CGFloat(55.0)))
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        if(UIDevice.modelName.contains(find: "iPad") || UIApplication.shared.statusBarOrientation == .landscapeLeft || UIApplication.shared.statusBarOrientation == .landscapeRight) {
+            
+        } else {
+            PortraitSetup()
+            reloadImagineWords()
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        if(UIDevice.modelName.contains(find: "iPad") || UIApplication.shared.statusBarOrientation == .landscapeLeft || UIApplication.shared.statusBarOrientation == .landscapeRight) {
+            
+        } else {
+            PortraitSetup()
+            reloadImagineWords()
+        }
     }
 }
